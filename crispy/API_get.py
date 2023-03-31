1482eeda5a27acf7d8192f1a9c289c67641798eb
Mohan Reddy <thathireddymohanreddy999@gmail.com>
2023-03-31 11:10:32 +0530

    added sample API requests

--- crispy/API_get.py
+++ crispy/API_get.py
@@ -1,9 +1,21 @@
 import requests
 from pprint import pprint
 
-URL = "https://api.thedogapi.com/v1/breeds"
+# URL = "https://api.thedogapi.com/v1/breeds"
 
-response = requests.get(URL)
+BASE_URL = "https://api.thedogapi.com/{}"
+
+headers = {'Content-Type': 'application/json',
+           'x-api-key': 'live_RAbjMzLty32bWKc54yiImvD47xQ3NMCwKE7YhvH5cswNHhui3FXKx2ShCf4QDvBf'}
+
+data = {
+    "image_id": "GAmy2bg8G",
+    "sub_id": "my-user-1232",
+    "value": -1
+}
+response = requests.post(BASE_URL.format("v1/votes"), headers=headers, json=data)
+
+# requests.get(BASE_URL.format("v1/votes?sub_id=my-user-1232"), headers=headers)
 
 data = response.json()
 
